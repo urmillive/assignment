cd6141d709f83938fd839a5b2cb3003fcb671b43
urmillive <urmillive@gmail.com>
2025-05-06 22:33:22 +0530

    refactor: move PostgreSQL connection logic to db.js and remove from app.js

--- app.js
+++ app.js
@@ -2,16 +2,12 @@ const express = require('express');
 const morgan = require('morgan');
 const dotenv = require('dotenv');
 const app = express();
-const pool = require('./utils/db');
+
 const studentRoutes = require('./routes/studentRoutes');
 const notFound = require('./middlewares/notFound');
 const errorHandler = require('./middlewares/errorHandler');
 
 dotenv.config();
-
-pool.connect()
-    .then(() => console.log('Connected to PostgreSQL database'))
-    .catch(err => console.error('Connection error', err.stack));
 
 app.use(express.json());
 app.use(morgan('dev'));
--- utils/db.js
+++ utils/db.js
@@ -9,4 +9,8 @@ const pool = new Pool({
     port: process.env.DB_PORT
 });
 
+pool.connect()
+    .then(() => console.log('Connected to PostgreSQL database'))
+    .catch(err => console.error('Connection error', err.stack));
+
 module.exports = pool;
